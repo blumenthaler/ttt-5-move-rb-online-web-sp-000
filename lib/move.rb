2465afaf9e52432e8c0ceec9ef51633b90d8511a
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-09 15:08:31 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def update_array_at_with(array, index, value)
   array[index] = value
 end
-update_array_at_with(board, 0, "X")
+# update_array_at_with(board, 0, "X")
 
 def input_to_index(user_input)
   user_input.to_i - 1
@@ -13,7 +13,7 @@ end
 
 
 def move(board, index_number, players_character = "X")
-
+  update_array_at_with(board, 0, "X")
 end
 
 
